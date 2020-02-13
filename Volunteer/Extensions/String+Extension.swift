5101590d10f0d6d4c2dca56b40e014a51775297c
HadyHammad22 <hady.fci@gmail.com>
2020-02-13 03:31:51 -0800

    add some files in extension

---
+++ Volunteer/Extensions/String+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  String+Extension.swift
+//  Volunteer
+//
+//  Created by Hady Hammad on 2/13/20.
+//  Copyright © 2020 Hady Hammad. All rights reserved.
+//
+
+import Foundation
---
+++ Volunteer/Extensions/UIButton+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIButton+Extension.swift
+//  Volunteer
+//
+//  Created by Hady Hammad on 2/13/20.
+//  Copyright © 2020 Hady Hammad. All rights reserved.
+//
+
+import Foundation
